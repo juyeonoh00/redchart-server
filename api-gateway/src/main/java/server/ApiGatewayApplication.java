4f0a5d63e721102653057d9e138b6639913bb8ae
juyeon <oh_ju_yeon@naver.com>
2024-08-08 17:31:25 +0900

    init

---
+++ api-gateway/src/main/java/server/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableOpenApi
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
