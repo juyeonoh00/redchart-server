fc92d9ca0ed3cc51062244811f7f852797e27030
juyeon <oh_ju_yeon@naver.com>
2024-09-06 19:45:18 +0900

    fix: docker-compose
    프론트 전달용

--- newsfeed-service/src/main/java/server/service/NewsfeedService.java
+++ newsfeed-service/src/main/java/server/service/NewsfeedService.java
@@ -2,7 +2,6 @@ package server.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import server.dto.NewsfeedDTO;
 import server.feign.server.PostController;
@@ -18,6 +17,6 @@ public class NewsfeedService {
     public NewsfeedDTO getNewsfeedDetails(Long userId) {
         // redis에서 userId의 postId리스트를 꺼내옴
         // post에 api 요청
-        return postController.get
+        return new NewsfeedDTO();
     }
 }
